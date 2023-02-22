0a0162ebc91d9560db771dd6a94c38a5e9a7c73e
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-02-22 11:14:48 +0200

    feature: add javascript

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import './template.html';
 import './assets/styles/index.scss';
+
+const cursor = document.querySelector('.cursor-animation__cursor');
+let timeout;
+
+const cursorMove = (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.top = y + 'px';
+  cursor.style.left = x + 'px';
+  cursor.style.display = 'flex';
+
+  const mouseStopped = () => {
+    cursor.style.display = 'none';
+    clearTimeout(timeout);
+  };
+
+  timeout = setTimeout(mouseStopped, 4000);
+};
+
+const cursorOut = () => {
+  cursor.style.display = 'none';
+};
+
+document.addEventListener('mousemove', cursorMove);
+document.addEventListener('mouseout', cursorOut);
